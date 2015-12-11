b379aff207c58d5692bc9226bc2e91badfbde7e5
LAURA EVANS <LKSEVANS12@GMAIL.com>
2015-12-10 19:28:55 -0500

    new schema for new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210191313) do
+ActiveRecord::Schema.define(version: 20151210234010) do
 
   create_table "availability_slots", force: true do |t|
     t.integer  "booking_id",      null: false
@@ -52,6 +52,14 @@ ActiveRecord::Schema.define(version: 20151210191313) do
     t.integer  "user_id",             null: false
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+  end
+
+  create_table "messages", force: true do |t|
+    t.string   "body",        null: false
+    t.integer  "sender_id",   null: false
+    t.integer  "receiver_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "reviews", force: true do |t|
