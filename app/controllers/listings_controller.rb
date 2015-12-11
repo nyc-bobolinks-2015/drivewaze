6a538d4ada127624d0643c8883e7743046a6222b
Sam Purcell <email": null>
2015-12-11 12:38:37 -0500

    Add link to show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,6 +4,9 @@ class ListingsController < ApplicationController
     @listing = Listing.find_by(id: params[:id])
     @map_image = "https://maps.googleapis.com/maps/api/staticmap?center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=600x300&maptype=roadmap
 &markers=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&key=#{ENV['GMAP_STATIC_KEY']}"
+    if request.xhr?
+      return render partial: 'listing', locals:{listing: @listing}
+    end
   end
 
   def index
