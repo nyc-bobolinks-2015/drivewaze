1761d6537f0d0a0ed83c9ed29d22dd95989834a5
Sam Purcell <email": null>
2015-12-11 15:47:43 -0500

    Add basic styling

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,7 +3,7 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find_by(id: params[:id])
     @map_image = "https://maps.googleapis.com/maps/api/staticmap?
-                  center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=600x300&maptype=roadmap
+                  center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=500x500&maptype=roadmap
                   &markers=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&key=#{ENV['GMAP_STATIC_KEY']}"
 
     if request.xhr?
