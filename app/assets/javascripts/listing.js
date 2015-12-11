c3249e90d6ffd28e0e961ee228b915b5cb1f00f2
Sam Purcell <email": null>
2015-12-11 14:28:55 -0500

    Tidy up listing.js

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,30 +1,30 @@
 function initMap() {
-  // Create a map object and specify the DOM element for display.
-  window.map = new google.maps.Map(document.getElementById('map'), {
+  window.map = new google.maps.Map(document.getElementById('map'),
+   {
     center: {lat: 40.7060137, lng: -74.0110914},
     scrollwheel: false,
     zoom: 8
-  });
+    });
 }
 
 $(document).ready(function(){
   initMap();
- (function(){
+  (function(){
 
     $.ajax({
       method: 'get',
       url: '/listings'
-    }).done(function(response){
-      for(var i = 0; i < response.length; i++) {
-        var marker = new google.maps.Marker({
+      }).done(function(response) {
+        for(var i = 0; i < response.length; i++) {
+          var marker = new google.maps.Marker({
             map: window.map,
             position: {lat: response[i].latitude, lng: response[i].longitude},
             title: ("$" + String(response[0].hourly_price) + '.00')
-  });
-      }
-    }).fail(function(error){
+          });
+        }
+      }).fail(function(error){
       console.log(error)
-    })
+        })
   })();
 
   window.map.addListener('bounds_changed', function(event){
@@ -36,46 +36,47 @@ $(document).ready(function(){
 
     $.ajax({
       method: 'get',
-      url: '/search' + "?westBound=" + westBound + "&eastBound=" + eastBound + "&northBound=" + northBound + "&southBound=" + southBound
-    }).done(function(response){
-      for(var i = 0; i < response.length; i++) {
-        var marker = new google.maps.Marker({
-            map: window.map,
-            position: {lat: response[i].latitude, lng: response[i].longitude},
-            title: ("$" + String(response[0].hourly_price) + '.00'),
-            label: "$",
-            id: response[i].id
-        });
-          marker.addListener('click', function(event){
-            $.ajax({
-              method: 'get',
-              url: '/listings/' + this.id
-            }).done(function(response){
-              $(".single-listing").html(response);
-            })
-
-          });
-
-      }
-    }).fail(function(error){
-      console.log(error)
-    })
+      url: '/search' +
+           "?westBound=" + westBound +
+           "&eastBound=" + eastBound +
+           "&northBound=" + northBound +
+           "&southBound=" + southBound
+      }).done(function(response){
+          for(var i = 0; i < response.length; i++) {
+            var marker = new google.maps.Marker({
+              map: window.map,
+              position: {lat: response[i].latitude, lng: response[i].longitude},
+              title: ("$" + String(response[0].hourly_price) + '.00'),
+              label: "$",
+              id: response[i].id
+            });
+            marker.addListener('click', function(event){
+              $.ajax({
+                method: 'get',
+                url: '/listings/' + this.id
+              }).done(function(response){
+                $(".single-listing").html(response);
+               })
+            });
+           }
+        }).fail(function(error){
+          console.log(error)
+          })
   })
 
   $("#search-form").on('submit', function(event){
     event.preventDefault();
 
     $.ajax({
-      method: 'post',
-      url: $(event.target).attr('action'),
-      data: $(event.target).serialize(),
-      dataType: 'json'
-    }).done(function(response){
-      var newLatLong = new google.maps.LatLng(response.results[0]["geometry"]["location"]["lat"], response.results[0]["geometry"]["location"]["lng"])
-      window.map.setCenter(newLatLong)
-    }).fail(function(error){
-      console.log(error);
+        method: 'post',
+        url: $(event.target).attr('action'),
+        data: $(event.target).serialize(),
+        dataType: 'json'
+      }).done(function(response){
+        var newLatLong = new google.maps.LatLng(response.results[0]["geometry"]["location"]["lat"], response.results[0]["geometry"]["location"]["lng"])
+        window.map.setCenter(newLatLong)
+      }).fail(function(error){
+        console.log(error);
     });
   })
-
 });
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,8 +2,10 @@ class ListingsController < ApplicationController
   include HTTParty
   def show
     @listing = Listing.find_by(id: params[:id])
-    @map_image = "https://maps.googleapis.com/maps/api/staticmap?center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=600x300&maptype=roadmap
-&markers=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&key=#{ENV['GMAP_STATIC_KEY']}"
+    @map_image = "https://maps.googleapis.com/maps/api/staticmap?
+                  center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=600x300&maptype=roadmap
+                  &markers=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&key=#{ENV['GMAP_STATIC_KEY']}"
+
     if request.xhr?
       return render partial: 'listing', locals:{listing: @listing}
     end
@@ -22,8 +24,10 @@ class ListingsController < ApplicationController
     eastBound = params[:eastBound]
     southBound = params[:southBound]
     northBound = params[:northBound]
-    @listings = Listing.where('latitude >= ?', southBound).where('latitude <= ?', northBound)
-    .where('longitude >= ?', westBound).where('longitude <= ?', eastBound)
+    @listings = Listing.where('latitude >= ?', southBound)
+                .where('latitude <= ?', northBound)
+                .where('longitude >= ?', westBound)
+                .where('longitude <= ?', eastBound)
 
     return render json: @listings
   end
@@ -33,5 +37,4 @@ class ListingsController < ApplicationController
     @response = HTTParty.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{search_term}")
     render json: @response
   end
-
 end
