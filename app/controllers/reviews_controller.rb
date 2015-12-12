45ffb21a11866fef47c0d6eba18b0551ee56d632
Laura Evans <email": null>
2015-12-12 15:05:25 -0500

    can create new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,24 @@
 class ReviewsController < ApplicationController
 
   def new
+    params[:user_id] ? @reviewable = User.find(params[:user_id]) : @reviewable = Listing.find(params[:listing_id])
+    @review = Review.new
   end
 
   def create
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @review = @user.reviews.build(review_params)
+    else
+      @listing = Listing.find(params[:listing_id])
+      @review = @listing.reviews.build(review_params)
+    end
+    if @review.save
+      redirect_to root_path
+      flash[:notice] = "Your review has been saved!"
+    else
+      render :"reviews/new"
+    end
   end
 
   def edit
@@ -16,6 +31,12 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:review_body,:review_score).merge(user_id: current_user.id)
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :reviewable,polymorphic: true
 
+
 end
