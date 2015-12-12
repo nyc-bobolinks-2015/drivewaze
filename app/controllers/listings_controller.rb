743648027718070a2d00d4bd6392c466934e57e2
Sam Purcell <email": null>
2015-12-11 19:48:41 -0500

    Add info to listings show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,11 +3,11 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find_by(id: params[:id])
     @map_image = "https://maps.googleapis.com/maps/api/staticmap?
-                  center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=500x500&maptype=roadmap
+                  center=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&zoom=15&size=1000x1000&maptype=roadmap
                   &markers=#{@listing.street},#{@listing.city},#{@listing.state},#{@listing.zipcode}&key=#{ENV['GMAP_STATIC_KEY']}"
 
     if request.xhr?
-      return render partial: 'listing', locals:{listing: @listing}
+      return render partial: 'listing_preview', locals:{listing: @listing}
     end
   end
 
