87f6b9028b67baf016ece5fdacbf07788ffc1ce3
heylittlehouse <michael.r.landon@gmail.com>
2015-12-13 15:01:22 -0500

    Change callback redirect from Stripe dashboard to listings#new

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,9 +8,7 @@ class OmniauthCallbacksController < ApplicationController
       access_code: request.env["omniauth.auth"].credentials.token,
       publishable_key: request.env["omniauth.auth"].info.stripe_publishable_key
     })
-      # anything else you need to do in response..
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Stripe") if is_navigational_format?
+      redirect_to new_listing_path
     else
       session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
