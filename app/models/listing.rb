dfab58c83f771f973d4d5d35837d48488de936ca
heylittlehouse <michael.r.landon@gmail.com>
2015-12-14 08:42:36 -0500

    Add dependent destroy to user and listing model associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,10 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
-  has_many :bookings
-  has_many :availability_slots
-  has_many :reviews, as: :reviewable
+  has_many :bookings, dependent: :destroy
+  has_many :availability_slots, dependent: :destroy
+  has_many :reviews, as: :reviewable, dependent: :destroy
   #---zino
-  has_many :parking_slots
+  has_many :parking_slots, dependent: :destroy
 
   geocoded_by :address
   before_save :geocode
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
-  has_many :bookings
-  has_many :listings
-  has_many :reviews, as: :reviewable
-  has_many :favorites
-  has_many :favorite_listings, through: :favorites, source: :favorited, source_type: "Listing"
-  has_many :favorite_users, through: :favorites, source: :favorited, source_type: "User"
+  has_many :bookings, dependent: :destroy
+  has_many :listings, dependent: :destroy
+  has_many :reviews, as: :reviewable, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_listings, through: :favorites, source: :favorited, source_type: "Listing", dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :favorited, source_type: "User", depenent: :destroy
 end
