09be636cf9acaf61dc54f5b68097c801ad724ebb
Sam Purcell <email": null>
2015-12-15 18:05:48 -0500

    Add photo to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215212024) do
+ActiveRecord::Schema.define(version: 20151215225659) do
 
   create_table "bookings", force: true do |t|
     t.integer  "total",                      null: false
@@ -109,6 +109,10 @@ ActiveRecord::Schema.define(version: 20151215212024) do
     t.string   "provider"
     t.string   "uid"
     t.string   "access_code"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
