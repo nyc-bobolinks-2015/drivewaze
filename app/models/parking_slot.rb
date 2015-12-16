0c857059e5f42d48f873ae4f93d657d5310ed458
Sam Purcell <email": null>
2015-12-15 21:02:52 -0500

    Update vehicle class and slot type

--- app/models/parking_slot.rb
+++ app/models/parking_slot.rb
@@ -1,4 +1,7 @@
 class ParkingSlot < ActiveRecord::Base
 	has_many :time_slots
 	belongs_to :listing
+
+  VEHICLE_CLASSES = {0 => "Compact", 1 => "Full Size", 2 => "SUV" }
+  SLOT_TYPES = {0 => "Driveway", 1 => "Lawn Parking", 2 => "Street Side Parking", 3 => "Permit Parking"}
 end
