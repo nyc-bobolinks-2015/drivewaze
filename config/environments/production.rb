906538b8a4af7180a748afcc464dcafa290280db
heylittlehouse <michael.r.landon@gmail.com>
2015-12-17 08:46:51 -0500

    Add mail configuration for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'spcs-app.herokuapp.com'}
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
