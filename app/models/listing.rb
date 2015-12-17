f9af612f8092201329ecb13b2d56307d0847068d
heylittlehouse <michael.r.landon@gmail.com>
2015-12-16 19:56:43 -0500

    Add missing end on listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -27,6 +27,7 @@ class Listing < ActiveRecord::Base
     else
       return 0
     end
+  end
 
   def self.filter_time(input_listings,startDateInput,endDateInput)
     startDateBeginning=DateTime.parse(startDateInput).beginning_of_day
